1b6a664a2ad2a0fe984ece1b9f71ebd56a9eb4f8
Aden Tran <47045532+adeziio@users.noreply.github.com>
2021-11-22 05:53:25 +0000

    fixed skipped feature

--- music.py
+++ music.py
@@ -5,13 +5,10 @@ from youtube_dl import YoutubeDL
 class music(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
-    
     self.is_playing = False
-
     self.music_queue = []
     self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
     self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
     self.vc = ""
 
   def search_yt(self, item):
@@ -27,70 +24,75 @@ class music(commands.Cog):
       self.is_playing = True
 
       m_url = self.music_queue[0][0]['source']
+      m_title = self.music_queue[0][0]['title']
+      # self.set_status(discord.ActivityType.listening, m_title)
 
+      print("play_next", len(self.music_queue))
       self.music_queue.pop(0)
-      self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
-    else:
-      self.is_playing = False
+      self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=self.play_next())
+    
 
   async def play_music(self):
     if len(self.music_queue) > 0:
       self.is_playing = True
 
       m_url = self.music_queue[0][0]['source']
+      m_title = self.music_queue[0][0]['title']
+      # await self.set_status(discord.ActivityType.listening, m_title)
 
-      if self.vc == "" or not not self.vc.is_connected():
+      if self.vc == "":
         try:
           self.vc = await self.music_queue[0][1].connect()
         except Exception as e:
           print(e)
-      else:
-        try:
-          self.vc = await self.bot.move_to(self.music_queue[0][1])
-        except Exception as e:
-          print(e)
-        
+      
+      print("play_music", len(self.music_queue))
       self.music_queue.pop(0)
-      self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
-    else:
-      self.is_playing = False
+      self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=self.play_next())
+
+
+  async def set_status(self, type, name):
+    await self.bot.change_presence(activity=discord.Activity(type=type, name=name))
 
   @commands.command()
   async def p(self, ctx, *args):
     query = " ".join(args)
-
-    voice_channel = ctx.author.voice.channel
-
-    if voice_channel is None:
+    voice_channel = ""
+    try:
+      voice_channel = ctx.author.voice.channel
+    except Exception:
       await ctx.send("Connect to a voice channel!")
-    else:
+
+    if voice_channel != "":
       song = self.search_yt(query)
       if type(song) == type(True):
         await ctx.send("Could not download the song. Incorrect format.")
       else:
-        await ctx.send("*" + query + " added to queue*")
+        await ctx.send("Added to queue ▶️: " + "*" + str(song['title']) + "*")
         self.music_queue.append([song, voice_channel])
 
         if self.is_playing == False:
-          await ctx.send("*Now playing: " + str(song['title']) + "*")
+          # await self.set_status(discord.ActivityType.listening, str(song['title']))
           await self.play_music()
 
   @commands.command()
   async def q(self, ctx):
-    retval = ""
-    for i in range(0, len(self.music_queue)):
-      retval += self.music_queue[i][0]['title'] + "\n"
-
-    if retval != "":
-      await ctx.send(retval)
+    if len(self.music_queue) > 0:
+      retval = "Up next ⏯:\n"
+      for i in range(0, len(self.music_queue)):
+        retval += "*" + self.music_queue[i][0]['title'] + "*\n"
+      if retval != "":
+        await ctx.send(retval)
     else:
       await ctx.send("Music queue is empty.")
 
   @commands.command()
   async def s(self, ctx):
     if self.vc != "":
+      if len(self.music_queue) == 0:
+        self.is_playing = False
       self.vc.stop()
-      await self.play_music()
+      self.play_next()
 
   @commands.command()
   async def m(self, ctx):
