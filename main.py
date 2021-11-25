3f2c809f48fe2309be2112647549e244cd5cc1e8
Aden Tran <47045532+adeziio@users.noreply.github.com>
2021-11-25 00:04:45 +0000

    remove space

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ async def change_status():
     musicObjActivity = musicObj.getActivity()
     await Bot.change_presence(status=musicObj.getStatus(), activity=musicObj.getActivity())
     await musicObj.displaySongInfo("Now Playing ▶️", musicObj.getSongInfo(), "green")
-    
 
   if musicObj.getIsPlaying() == False:
     await Bot.change_presence(status=musicObj.getStatus(), activity=discord.Activity(type=discord.ActivityType.listening, name="Spotify"))
